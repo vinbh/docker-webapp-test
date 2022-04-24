1f0fc4a7cf44bc2bdeb12762943092342dd8cf8b
vinbh <vinayak.farcry@gmail.com>
2022-04-24 18:08:14 +0530

    sample app.py, and templated folders for base html and jinja files for repeating post and title

--- app.py
+++ app.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python3
 
-from datetime import datetime
+import sqlite3
+from flask import Flask, render_template
 
-from flask import Flask
 app = Flask(__name__)
 
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
+
 @app.route('/')
-def check():
-   now = datetime.now() 
-   dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
-   return( dt_string)	 
-
-
-if __name__ == '__main__':
-    app.run()
+def index():
+    conn = get_db_connection()
+    posts = conn.execute('SELECT * FROM posts').fetchall()
+    conn.close()
+    return render_template('index.html', posts=posts)
